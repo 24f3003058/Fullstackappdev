4287be0b9b8b9de1c9408a8252500c88f5c8b313
24f3003058 <24f3003058@ds.study.iitm.ac.in>
2026-01-08 10:48:35 +0530

    Add isPalindrome function in palindrome.js
    Implement a case-insensitive palindrome checker.

---
+++ Week0/palindrome.js
@@ -0,0 +1,16 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  const str1=str.toLowerCase();
+  const str2=str1.split("").reverse().join("");
+  if(str1==str2){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log(isPalindrome("Nan"))
