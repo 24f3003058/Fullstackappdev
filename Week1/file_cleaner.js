88e68aa4cfee6f59238283231f5923d45f506533
24f3003058 <24f3003058@ds.study.iitm.ac.in>
2026-01-14 05:01:42 +0530

    Add file cleaner to remove extra spaces from text
    Implement a file cleaner that removes extra spaces from a file.

---
+++ Week1/file_cleaner.js
@@ -0,0 +1,21 @@
+// ## File cleaner
+// Read a file, remove all the extra spaces and write it back to the same file.
+// For example, if the file input was
+// hello     world    my    name   is       raman
+// After the program runs, the output should be
+// hello world my name is raman
+
+const fs=require('fs');
+let p=new Promise(function(resolve){
+  fs.readFile('a.txt','utf-8',function(err,data){
+    data=data.replace(/\s+/g,' ');
+    resolve(data);
+  })
+});
+
+async function main(){
+  let data=await p;
+  console.log(data);
+}
+
+main();
