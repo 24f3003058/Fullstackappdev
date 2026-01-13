64c406b94db52163b0ed9982f2627833c867b83a
24f3003058 <24f3003058@ds.study.iitm.ac.in>
2026-01-14 04:38:10 +0530

    Refactor comments in readFile.js
    Commented out instructions for reading a file and performing async tasks.

---
+++ Week1/readFile.js
@@ -0,0 +1,24 @@
+// ## Reading the contents of a file
+
+// Write code to read contents of a file and print it to the console. 
+// You can use the fs library to as a black box, the goal is to understand async tasks. 
+// Try to do an expensive operation below the file read and see how it affects the output. 
+// Make the expensive operation more and more expensive and see how it affects the output. 
+
+const fs=require('fs');
+let p =new Promise (function(resolve){
+  fs.readFile('a.txt','utf-8',function(err,data){
+    resolve(data)
+  });
+});
+async function main(){
+  let data=await p;
+  console.log(data);
+}
+main();
+
+let c=0;
+for(let i=0;i<100000000;i++){
+  c+=i;
+}
+console.log(c);
